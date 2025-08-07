3dee0763a844eb63c531551465c6a40ac910e077
marketlubes@gmail.com <marketlubedevelopers@gmail.com>
2025-08-07 16:17:12 +0530

    Update project structure: rename package, add routing, and implement homepage with animations. Remove unused sections and add new icons for homepage display.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Homepage/HomePage";
 
 function App() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Homepage/HomePage.jsx
@@ -0,0 +1,76 @@
+import eIcon from "../../assets/icons/e.svg";
+import iIcon from "../../assets/icons/i.svg";
+import kIcon from "../../assets/icons/k.svg";
+import aIcon from "../../assets/icons/a.svg";
+import sIcon from "../../assets/icons/s.svg";
+import seconsI from "../../assets/icons/seconsI.svg";
+import seconda from "../../assets/icons/seconda.svg";
+
+import adA from "../../assets/icons/letter_a_0_nobg.svg";
+import adD from "../../assets/icons/letter_d_1_nobg.svg";
+import adV from "../../assets/icons/letter_v_2_nobg.svg";
+import adE from "../../assets/icons/letter_e_3_nobg.svg";
+import adR from "../../assets/icons/letter_r_4_nobg.svg";
+import adT from "../../assets/icons/letter_t_5_nobg.svg";
+import adI from "../../assets/icons/letter_i_6_nobg.svg";
+import adN from "../../assets/icons/letter_n_9_nobg.svg";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="homepage-container">
+        <div className="homepage-container-top">
+          <div>
+            <img src={eIcon} alt="e" className="letter-e" />
+          </div>
+          <div>
+            <img src={iIcon} alt="i" className="letter-i" />
+          </div>
+          <div>
+            <img src={kIcon} alt="k" className="letter-k" />
+          </div>
+          <div>
+            <img src={aIcon} alt="a" className="letter-a" />
+          </div>
+          <div>
+            <img src={sIcon} alt="s" className="letter-s" />
+          </div>
+          <div>
+            <img src={seconsI} alt="i" className="letter-i-second" />
+          </div>
+          <div>
+            <img src={seconda} alt="a" className="letter-a-second" />
+          </div>
+        </div>
+        <div className="homepage-container-bottom">
+          <div>
+            <img src={adA} alt="a" className="ad-letter-a" />
+          </div>
+          <div>
+            <img src={adD} alt="d" className="ad-letter-d" />
+          </div>
+          <div>
+            <img src={adV} alt="v" className="ad-letter-v" />
+          </div>
+          <div>
+            <img src={adE} alt="e" className="ad-letter-e" />
+          </div>
+          <div>
+            <img src={adR} alt="r" className="ad-letter-r" />
+          </div>
+          <div>
+            <img src={adT} alt="t" className="ad-letter-t" />
+          </div>
+          <div>
+            <img src={adI} alt="i" className="ad-letter-i" />
+          </div>
+          <div>
+            <img src={adN} alt="n" className="ad-letter-n" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
